30047fe1aa281e2222deffe5fb6423ec524cc8b2
NirBer48 <nir.berkovich@gmail.com>
2023-12-03 07:58:51 +0200

    fix(Translator.py): add more languages to translate

--- Translator.py
+++ Translator.py
@@ -6,23 +6,26 @@ from deep_translator import GoogleTranslator
 r = sr.Recognizer() 
 engine = pyttsx3.init()
 
-languages = {"en": "en_US",
-             "he": "he_IL",
-             "zh": "zh-TW",
-             "ar": "ar-SA"}
+languages = {"en": ["English", "en-US"], # English
+             "ja": ["Japanese", "ja-JP"], # Japanese
+             "he": ["Hebrew", "he-IL"], # Hebrew
+             "zh": ["Chinese", "zh-TW"], # Chinese
+             "ar": ["Arabic", "ar-SA"]} # Arabic
 
+fromLang = "he"
+toLang = "ja"
 
 def change_voice(language):
     for voice in engine.getProperty('voices'):
-        if language in voice.languages:
+        if language in voice.name:
             engine.setProperty('voice', voice.id)
 
             return True
 
 
 def speak_text(command, lang):
-    change_voice(lang)
-    translated = GoogleTranslator(source='auto', target=lang[:2]).translate(command)
+    change_voice(languages[lang][0])
+    translated = GoogleTranslator(source='auto', target=lang).translate(command)
     engine.say(translated) 
     engine.runAndWait()
 
@@ -53,4 +56,6 @@ def get_speech(lang):
         print("unknown error occurred")
 
 
-speak_text(get_speech(languages["he"]), 'en')
+if __name__ == "__main__":
+    speechText = get_speech(languages[fromLang][1])
+    speak_text(speechText, toLang)
